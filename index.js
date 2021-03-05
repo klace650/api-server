35e7e6a8afaea5b554c03c6348fd62b0323eee6f
Klace Koch <koch.klace@gmail.com>
2021-03-04 21:36:11 -0800

    killed server - created model

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
-
-const express = require('express');
-const app = express();
-
 const mongoose = require('mongoose');
+
+const ModelOne = require('./src/models/model1.js');
+
 const MONGODB_URI = 'mongodb://localhost:27017/api-serverdb';
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.listen(3000, () => {
-    console.log('Server is on or something');
-});
+let connectToDB = async () => {
+  let sample = {
+      name: 'jay',
+      color: 'blue',
+      size: 4
+  };
+  let saveSample = new ModelOne(sample);
+  await saveSample.save();
+  console.log(saveSample);
+};
+
+connectToDB();
+
+// let jay = await ModelOne.findById()
